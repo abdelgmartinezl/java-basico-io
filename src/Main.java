63276ca12462464dc624b989f6d99bda3d10486b
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2025-02-01 15:49:42 -0500

    Ya terminamos la clase.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.printf("Habla pelao!");
+        System.out.println("Xopa, pelao!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Este es un comentario de una linea
+        /*
+           Esto es un comentario
+           de varias lineas :-)
+        */
+
+        int edad;
+        edad = 100;
+
+        byte num = 58;
+        short año = 2025;
+        int poblacion = 1000000; // Este es el que debo usar
+        long estrellas = 900000000L;
+
+        float temperatura = 35.3f;
+        double no_pi = 3.123423424; // Este es el que debo usar
+
+        char inicial = 'A';
+        String nombre = "Anacleta";
+
+        boolean esJavaDivertido = false;
+
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("Año: " + año);
+        System.out.println("Poblacion: " + poblacion + " personas");
+        System.out.println("Estrellas: " + estrellas + " estrellas");
+        System.out.println("Temperatura: " + temperatura + "º");
+        System.out.println("No PI: " + no_pi);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("EsJavaDivertido: " + esJavaDivertido + ", mentira!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce tu nombre: ");
+        nombre = sc.nextLine();
+
+        System.out.print("Introduce tu edad: ");
+        edad = sc.nextInt();
+
+        System.out.print("Introduce tu altura: ");
+        double altura = sc.nextDouble();
+
+        System.out.println("Resumen de Datos");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+
+        sc.close();
     }
 }
